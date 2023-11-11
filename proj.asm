9166ec8f5077bb967789f21ea5e2ec0b2c425617
SlendyGamer <gabrielcarotti@hotmail.com>
2023-11-11 19:26:58 -0300

    codigo indo ali

--- proj.asm
+++ proj.asm
@@ -16,7 +16,7 @@
         PUSH CX                      ;CX ESTA EM USO MAS PRECISAREMOS UTILIZAR UM LOOP ABAIXO PARA RECEBER 3X NOTAS, PORTANTO SALVO SEU VALOR NA PILHA
         MOV CX, 3
 
-        PUSH BX                  ;MESMA SITUAÇÃO DE CX, BX CONTEM A INFORMAÇÃO DE QUE LINHA ESTAMOS, PORTANTO NAO PODEMOS PERDER ESSE VALOR      
+        PUSH BX                  ;MESMA SITUA??O DE CX, BX CONTEM A INFORMA??O DE QUE LINHA ESTAMOS, PORTANTO NAO PODEMOS PERDER ESSE VALOR      
         MOV SI, 34      ;PARA PODER DEFINIR SI COMO ELEMENTO 34, PRECISAMOS DEFINIR POR MEIO DE BX COMO PONTEIRO BASE             
 
         POP BX                   
@@ -47,14 +47,14 @@
 
     MSGNOTA DB "NOTA DO ALUNO: $"
 
-    TABELA DB 32, ?, 32 DUP('$'), 4 DUP(?)          ;QUANTIDADE DE CARACTERES DA STRING EM TABELA + 1 (NAO CONTA O ENTER)
-           DB 32, ?, 32 DUP('$'), 4 DUP(?)          ;STRING EM TABELA + 2 (MAX. 30 CARACTERES + ENTER + $)
-           DB 32, ?, 32 DUP('$'), 4 DUP(?)          ;NOTAS EM  TABELA + 34, 35 E 36
-           DB 32, ?, 32 DUP('$'), 4 DUP(?)          ;MEDIA EM  TABELA + 37
-           DB 32, ?, 32 DUP('$'), 4 DUP(?)          ;NOVA LINHA DE 38 EM 38 (TABELA + 38) // DIRETAMENTE ABAIXO DO ELEMENTO QUE ESTA SENDO APONTADO
+    TABELA DB 5 DUP (32, ?, 32 DUP('$'), 4 DUP(?))          ;QUANTIDADE DE CARACTERES DA STRING EM TABELA + 1 (NAO CONTA O ENTER). STRING EM TABELA + 2 (MAX. 30 CARACTERES + ENTER + $). NOTAS EM  TABELA + 34, 35 E 36.
+                    ;32, ?, 32 DUP( $ ), 4 DUP(?)                                       ;MEDIA EM  TABELA + 37.NOVA LINHA DE 38 EM 38 (TABELA + 38) // DIRETAMENTE ABAIXO DO ELEMENTO QUE ESTA SENDO APONTADO.
+                    ;32, ?, 32 DUP( $ ), 4 DUP(?)
+                    ;32, ?, 32 DUP( $ ), 4 DUP(?)
+                    ;32, ?, 32 DUP( $ ), 4 DUP(?)
 
     MENUPRINC DB 'O QUE DESEJA FAZER?',10,13
-        Q     DB '1 - VER TABELA',10,13
+              DB '1 - VER TABELA',10,13
               DB '2 - EDITAR NOTAS',10,13
               DB '0 - SAIR',10,13,'$'
 
@@ -62,6 +62,10 @@
              DB '1 - NOMES',10,13
              DB '2 - NOTAS',10,13
              DB '0 - RETORNAR',10,13,'$'
+             
+    PESQUISA DB 32, ?, 32 DUP('$')
+    
+    FFLUSH DB 32, ?, 32 DUP('$')
 
     ESPACO DB ' $'
 
@@ -73,7 +77,7 @@
         MOV ES, AX
 
         XOR BX, BX                  ;ZERA BX PARA TER CERTEZA QUE FOI INICIADO COM 0
-        MOV CX, 5                   ;INICIA CONTADOR EM 5, POIS RECEBERA A INFORMAÇÃO DE 5 ESTUDANTES
+        MOV CX, 5                   ;INICIA CONTADOR EM 5, POIS RECEBERA A INFORMA??O DE 5 ESTUDANTES
 
 
     LOOPNOMES:   
@@ -83,19 +87,13 @@
         LEA DX, MSGNOME                ;IMPRIME A MENSAGEM EM 'NOME'
         PRINTSTRING                   
 
-        LEA DX, TABELA + BX          ;APONTA PARA TABELA + BX (BX INDICA QUAL NOME IRA RECEBER(1º, 2º, 3º...))(BX INICIA C/ 0 E SERA INCREMENTADO DE 38 EM 38)
+        LEA DX, TABELA + BX          ;APONTA PARA TABELA + BX (BX INDICA QUAL NOME IRA RECEBER(1?, 2?, 3?...))(BX INICIA C/ 0 E SERA INCREMENTADO DE 38 EM 38)
         CALL RECEBE_NOME
 
         PULALINHA
 
-        ;PREP_NOTA3X
-                            PUSH CX                      ;CX ESTA EM USO MAS PRECISAREMOS UTILIZAR UM LOOP ABAIXO PARA RECEBER 3X NOTAS, PORTANTO SALVO SEU VALOR NA PILHA
-                            MOV CX, 3
-
-                            PUSH BX                  ;MESMA SITUAÇÃO DE CX, BX CONTEM A INFORMAÇÃO DE QUE LINHA ESTAMOS, PORTANTO NAO PODEMOS PERDER ESSE VALOR      
-                            MOV SI, 34      ;PARA PODER DEFINIR SI COMO ELEMENTO 34, PRECISAMOS DEFINIR POR MEIO DE BX COMO PONTEIRO BASE             
-
-                            POP BX                         
+        PREP_NOTA3X
+                       
     RECEBENOTA:
         LEA DX, MSGNOTA             ;IMPRIME A MENSAGEM EM 'NOME'
         PRINTSTRING
@@ -108,7 +106,7 @@
         CALL CALCMEDIA
 
 
-        ADD BX, 37                  ;APONTA PARA A PROXIMA LINHA DA MATRIZ(PROXIMO ALUNO)
+        ADD BX, 38                  ;APONTA PARA A PROXIMA LINHA DA MATRIZ(PROXIMO ALUNO)
     LOOP LOOPNOMES
 
     RETURN:
@@ -121,7 +119,10 @@
         MOV AH, 01
         SELECT_MENU:
 
-        INT 21H                                     ;RECEBE OPÇÃO SELECINADA PELO USUÁRIO
+        INT 21H                                     ;RECEBE OP??O SELECINADA PELO USU?RIO
+
+        CMP AL, '0'                                 ;SE FOR 0
+        JE EXIT                             ;PULA PARA HEX_ENTRADA
 
         CMP AL, '1'                                 ;SE FOR 1
         JE VIEW                             ;PULA PARA BIN_ENTRADA
@@ -129,27 +130,34 @@
         CMP AL, '2'                                 ;SE FOR 2
         JE EDIT                              ;PULA PARA DEC_ENTRADA
 
-        CMP AL, '0'                                 ;SE FOR 3
-        JE EXIT                             ;PULA PARA HEX_ENTRADA
+        JMP SELECT_MENU                          ;SE FOR QUALQUER OUTRA COISA AL?M DE 1,2 OU 3, PEDE INPUT NOVAMENTE
+        
+    EXIT:
 
-        JMP SELECT_MENU                          ;SE FOR QUALQUER OUTRA COISA ALÉM DE 1,2 OU 3, PEDE INPUT NOVAMENTE
+        mov ah,4ch          ; funcao de termino
+        int 21h
 
     VIEW:
         
+        PULALINHA
+        PULALINHA
+        
         CALL SAITABELA
         JMP RETURN
+        
 
     EDIT:
 
         PULALINHA
 
         LEA DX, MENUPESQ
+
         PRINTSTRING
 
         MOV AH, 01
         SELECT_PESQ:
 
-        INT 21H                                     ;RECEBE OPÇÃO SELECINADA PELO USUÁRIO
+        INT 21H                                     ;RECEBE OP??O SELECINADA PELO USU?RIO
 
         CMP AL, '0'                                 ;SE FOR 3
         JE RETURN                             ;PULA PARA HEX_ENTRADA
@@ -160,18 +168,17 @@
         CMP AL, '2'                                 ;SE FOR 2
         JE PESQNOTA                              ;PULA PARA DEC_ENTRADA
 
-        JMP SELECT_PESQ                          ;SE FOR QUALQUER OUTRA COISA ALÉM DE 1,2 OU 3, PEDE INPUT NOVAMENTE
+        JMP SELECT_PESQ                          ;SE FOR QUALQUER OUTRA COISA AL?M DE 1,2 OU 3, PEDE INPUT NOVAMENTE
 
     PESQNOME:
-
+    
+        CALL PESQ_NOME
+        JMP RETURN
+        
     PESQNOTA:
 
-
-
-    EXIT:
-
-        mov ah,4ch          ; funcao de termino
-        int 21h
+        ;CALL PESQ_NOTA
+        JMP RETURN
 
     MAIN ENDP
 
@@ -179,7 +186,7 @@
 
 
     RECEBE_NOME PROC
-        ;FUNÇÃO PERMITE O USUARIO DIGITAR UM NOME, EDITANDO O COM BACKSPACE, SEM PROBLEMAS
+        ;FUN??O PERMITE O USUARIO DIGITAR UM NOME, EDITANDO O COM BACKSPACE, SEM PROBLEMAS
         ;NAO RECEBE PARAMETROS
         ;DEVOLVE A STRING DIGITADA E A QUANTIDADE DE CARACTERES DIGITADOS
         PUSH BX
@@ -187,14 +194,14 @@
         PUSH DX
 
         MOV AH, 0AH                 ;RECEBERA UM STRING
-        INT 21H                     ;RECEBE A STRING DO USUÁRIO
-        LEA BX, TABELA + BX ; BX = Número de caracteres digitados - 1 (não conta Enter)
+        INT 21H                     ;RECEBE A STRING DO USU?RIO
+        LEA BX, TABELA + BX ; BX = N?mero de caracteres digitados - 1 (n?o conta Enter)
 
         ; Move BX para o fim da string
         INC BX          ;aponta 1 casa depois do ultimo caracter *contado*(enter)
         MOV DL, [BX]    ;RECEBE N DE CARACTERES DIGITADOS - 1(ENTER)
         INC BX
-        ADD BX, DX      ;APONTA BX PARA A MESMA POSIÇÃO PARA PODER INSERIR NA MEMORIA
+        ADD BX, DX      ;APONTA BX PARA A MESMA POSI??O PARA PODER INSERIR NA MEMORIA
         MOV DL, '$'
 
         ; troca enter por $
@@ -210,16 +217,16 @@
 
 
     ENTNOTAS PROC
-        ;UTILIZA BX PARA APONTAR PARA A LINHA DO RESPECTIVO ALUNO NA MATRIZ, PRMITINDO A INSERÇAO DA NOTA NO LUGAR CORRETO (0, 38, ...)
+        ;UTILIZA BX PARA APONTAR PARA A LINHA DO RESPECTIVO ALUNO NA MATRIZ, PRMITINDO A INSER?AO DA NOTA NO LUGAR CORRETO (0, 38, ...)
         ;UTILIZA SI PARA APONTAR PARA O ELEMENTO CORRETO DAS NOTAS NA LINHA (34, 35, 36)
-        ;CHAMA DENTRO DE SI A FUNÇÃO PARA RECEBER NOTAS, ENTDEC
-        ;SI É DEVOLVIDO COM A POSIÇÃO DO ELEMENTO 37 NA MATRIZ, PARA FACILITAR A INSERÇÃO DAS MÉDIAS NESSA POSIÇÃO.
+        ;CHAMA DENTRO DE SI A FUN??O PARA RECEBER NOTAS, ENTDEC
+        ;SI ? DEVOLVIDO COM A POSI??O DO ELEMENTO 37 NA MATRIZ, PARA FACILITAR A INSER??O DAS M?DIAS NESSA POSI??O.
         PUSH BX
         
         LEA BX, TABELA + BX         ;APONTA PARA A LINHA DO RESPECTIVO ALUNO
         CALL ENTDEC
 
-        MOV [BX + SI], AX
+        MOV [BX + SI], AL
         INC SI
 
         POP BX
@@ -232,22 +239,22 @@
     ENTDEC PROC
         ;NAO RECEBE PARAMETROS
         ;FAZ O USUARIO INSERIR UM INPUT NUMERICO, NA FORMA DE CARACTERE, DEVOLVENDO O SEU RESPECTIVO VALOR EM AX, NA FORMA DE NUMERO
-        ;CONVERTE O VALOR RECEBIDO PARA NUMERO POR MEIO DE MULTIPLICAÇÕES SUCESSIVAS POR 10
+        ;CONVERTE O VALOR RECEBIDO PARA NUMERO POR MEIO DE MULTIPLICA??ES SUCESSIVAS POR 10
         PUSH BX
-        XOR BX, BX                                  ;ZERA BX POIS ELE SERA UTILIZADO PARA A DIVISÃO NA FUNÇÃO
+        XOR BX, BX                                  ;ZERA BX POIS ELE SERA UTILIZADO PARA A DIVIS?O NA FUN??O
 
     RECEBEDEC:
-        MOV AH, 01                                  ;COMO AH É LIMPO LOGO A FRENTE, REDEFINO ELE AQUI.
+        MOV AH, 01                                  ;COMO AH ? LIMPO LOGO A FRENTE, REDEFINO ELE AQUI.
         INT 21H                                     
 
         CMP AL, 13                                  ;COMPARA VALOR RECEBIDO COM CARRIAGE RETURN
         JE ENTDECFIM                                ;SE FOR, FINALIZA O INPUT
 
         CMP AL, '0'                                 ;SE FOR MENOR QUE '0'
-        JB RECEBEDEC                                ;PEDE NOVO INPUT, POIS VALOR É INVALIDO
+        JB RECEBEDEC                                ;PEDE NOVO INPUT, POIS VALOR ? INVALIDO
 
         CMP AL, '9'                                 ;SE FOR MAIOR QUE '9'
-        JA RECEBEDEC                                ;PEDE NOVO INPUT, POIS VALOR É INVALIDO
+        JA RECEBEDEC                                ;PEDE NOVO INPUT, POIS VALOR ? INVALIDO
 
     DECPARABIN:
         XOR AH, AH                                  ;LIMPO AH, PARA EVITAR QUE BX MULTIPLIQUE UM LIXO INDESEJADO
@@ -255,7 +262,7 @@
         AND AL, 0FH                                 ;TRANSFORMA NUMERO(CARACTERE) EM BINARIO
         PUSH AX                                     ;SALVA NUMERO NA PILHA
 
-        MOV AX, 10                                  ;AX RECEBE 10 PARA FAZER A CONTA (AX=10*BX+INPUT) OBS: O 10 É O AX
+        MOV AX, 10                                  ;AX RECEBE 10 PARA FAZER A CONTA (AX=10*BX+INPUT) OBS: O 10 ? O AX
         MUL BX                                      ;REALIZA AX * BX E GUARDA VALOR EM AX (AX=AX*BX), ISSO ABRE ESPACO PARA JUNTAR OS NUMEROS
         POP BX                                      ;RECUPERA VALOR DE AX NA PILHA E COLOCA-O EM BX
         ADD BX, AX                                  ;SOMA BX + AX E GUARDA EM BX (BX=BX+AX)
@@ -263,7 +270,7 @@
         JMP RECEBEDEC                               ;RECEBE MAIS UM INPUT
 
     ENTDECFIM:
-        MOV AX, BX                                  ;SALVA EM AX O VALOR FINAL NOVAMENTE, PARA RETORNÁ-LO À FUNÇÃO
+        MOV AX, BX                                  ;SALVA EM AX O VALOR FINAL NOVAMENTE, PARA RETORN?-LO ? FUN??O
 
         POP BX
         RET
@@ -273,14 +280,8 @@
 
 
     CALCMEDIA PROC
-    ;PREP_NOTA3X
-        PUSH CX                      ;CX ESTA EM USO MAS PRECISAREMOS UTILIZAR UM LOOP ABAIXO PARA RECEBER 3X NOTAS, PORTANTO SALVO SEU VALOR NA PILHA
-        MOV CX, 3
+        PREP_NOTA3X
 
-        PUSH BX                  ;MESMA SITUAÇÃO DE CX, BX CONTEM A INFORMAÇÃO DE QUE LINHA ESTAMOS, PORTANTO NAO PODEMOS PERDER ESSE VALOR      
-        MOV SI, 34      ;PARA PODER DEFINIR SI COMO ELEMENTO 34, PRECISAMOS DEFINIR POR MEIO DE BX COMO PONTEIRO BASE             
-
-        POP BX
         
         XOR AX, AX              ;LIMPA A ULTIMA NOTA DE AX
 
@@ -293,7 +294,7 @@
 
     LOOP LOOPSOMA
 
-        PUSH BX                 ;SALVA BX NOVAMENTE POIS OCORRERA A DIVISÃO DA MEDIA
+        PUSH BX                 ;SALVA BX NOVAMENTE POIS OCORRERA A DIVIS?O DA MEDIA
         XOR DX, DX              ;GARANTE QUE DX ESTA LIMPO PARA EVITAR OVERFLOW
 
         MOV BX, 3               ;DEFINE O VALOR DO DIVISOR
@@ -301,7 +302,7 @@
 
         POP BX                  ;RECUPERA O VALOR DE BX QUE APONTA PARA A LINHA
 
-        MOV [BX + SI], AX       ;COPIA PARA A POSIÇÃO DA MATRIZ(BX + 37) O VALOR DA MEDIA FINAL
+        MOV [BX + SI], AL       ;COPIA PARA A POSI??O DA MATRIZ(BX + 37) O VALOR DA MEDIA FINAL
 
         POP BX                  ;RECUPERA O VALOR DE BX ORIGINAL
         POP CX                  ;RETORNA O CONTADOR AO SEU VALOR ORIGINAL
@@ -314,7 +315,6 @@
     SAITABELA PROC
         LEA BX, TABELA
         MOV CX, 5
-        PULALINHA
     SAILINHA:
         MOV SI, 34                         
         MOV DX, BX
@@ -331,18 +331,13 @@
 
     SAINOTA:
         CALL SAIDEC
-                                                MOV AX, [BX + SI]
-                                                MOV DL, AL
-                                                OR DL, 30H
-                                                MOV AH, 02
-                                                INT 21H
 
         ESPACA
 
         INC SI
     LOOP SAINOTA
         POP CX
-        ADD BX, 37
+        ADD BX, 38
         PULALINHA
     LOOP SAILINHA
         RET
@@ -355,24 +350,25 @@
                 PUSH BX
                 PUSH CX
                 PUSH DX
-                ;RECEBE DA MAIN O ENDEREÇO MATRICIAL [BX+SI] COM O VALOR PARA SER IMPRESSO
+                ;RECEBE DA MAIN O ENDERE?O MATRICIAL [BX+SI] COM O VALOR PARA SER IMPRESSO
                 ;NAO DEVOLVE NADA PARA MAIN
-                    MOV AX, [BX + SI]
+                    XOR AX, AX
+                    MOV AL, [BX + SI]
                 PREPARADIV:
                     XOR CX, CX                                  ;ZERA CX POIS ELE SERA USADO COMO CONTADOR
-                    MOV BX, 10                                  ;DEFINE BX COMO 10, JÁ QUE AS DIVISÕES SERÃO POR 10
+                    MOV BX, 10                                  ;DEFINE BX COMO 10, J? QUE AS DIVIS?ES SER?O POR 10
 
                 SEPARADEC:
-                    XOR DX, DX                                  ;ZERA DX, POIS DX CONTERÁ O RESTO E SE NÃO FOR LIMPADO A CADA DIVISÃO, PODE CAUSAR OVERFLOW
+                    XOR DX, DX                                  ;ZERA DX, POIS DX CONTER? O RESTO E SE N?O FOR LIMPADO A CADA DIVIS?O, PODE CAUSAR OVERFLOW
                     
                     DIV BX                                      ;DIVIDE AX POR BX E GUARDA EM AX O QUOCIENTE E, EM DX, O RESTO
-                    PUSH DX                                     ;SALVA O VALOR DO RESTO NA PILHA, QUE SERÁ IMPRESSO DEPOIS
-                    INC CX                                      ;INCREMENTA O CONTADOR, ESSE SERÁ O NUMERO DE VEZES QUE A PILHA DEVERÁ SER ACESSADA PARA RECUPERAR OS VALORES DE DX SALVOS
+                    PUSH DX                                     ;SALVA O VALOR DO RESTO NA PILHA, QUE SER? IMPRESSO DEPOIS
+                    INC CX                                      ;INCREMENTA O CONTADOR, ESSE SER? O NUMERO DE VEZES QUE A PILHA DEVER? SER ACESSADA PARA RECUPERAR OS VALORES DE DX SALVOS
                     
                     OR AX, AX                                   ;SE AX = 0
-                    JNZ SEPARADEC                               ;SE AX != 0, CONTINUA COM AS DIVISÕES
+                    JNZ SEPARADEC                               ;SE AX != 0, CONTINUA COM AS DIVIS?ES
 
-                    MOV AH, 02                                  ;PREPARA PARA A IMPRESSÃO DO NUMERO DECIMAL
+                    MOV AH, 02                                  ;PREPARA PARA A IMPRESS?O DO NUMERO DECIMAL
 
                 RETORNALOOP:
                     POP DX                                      ;RECUPERA O VALOR DE DX DA PILHA
@@ -386,5 +382,20 @@
                 POP AX
                     RET                                         ;RETOMA O CONTROLE PARA A ROTINA
             SAIDEC ENDP
+            
 
+            
+            PESQ_NOME PROC
+                XOR BX, BX
+                LEA DX, TABELA + BX
+                ;ADD 
+                RET
+            PESQ_NOME ENDP
+            
+            
+            
+            PESQ_NOTA PROC
+            
+                RET
+            PESQ_NOTA ENDP
 END MAIN
